05a42afb1d0fcf731085a578363f5a63afc35528
DimitarShumanov <86767780+DimitarShumanov@users.noreply.github.com>
2023-04-26 11:07:37 +0300

    url change

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://spacepeackers.com';
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
 export const FOODS_BY_SEARCH_URL = FOODS_URL + '/search/';
