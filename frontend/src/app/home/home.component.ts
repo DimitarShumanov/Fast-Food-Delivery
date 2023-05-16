46fd8126db9191328be88c6b452cec2927eaadf1
DimitarShumanov <86767780+DimitarShumanov@users.noreply.github.com>
2023-05-16 10:38:04 +0300

    Added logo

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,35 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from 'src/app/services/food.service';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Food } from '../shared/models/Food';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   foods: Food[] = [];
-  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
-    let foodsObservalbe: Observable<Food[]>
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
+    let foodsObservalbe: Observable<Food[]>;
     activatedRoute.params.subscribe((params) => {
       if (params['searchTerm'])
-        foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+        foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(
+          params['searchTerm']
+        );
       else if (params['tag'])
         foodsObservalbe = this.foodService.getAllFoodsByTag(params['tag']);
-      else
-        foodsObservalbe = foodService.getAll();
+      else foodsObservalbe = foodService.getAll();
 
       foodsObservalbe.subscribe((serverFoods) => {
         this.foods = serverFoods;
-      })
-    })
-
+      });
+    });
   }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
